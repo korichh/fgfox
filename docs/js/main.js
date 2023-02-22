283ed802ee6b4fdb1e44357373b30b86bde8a816
korGetAv <korichhak@gmail.com>
2023-02-22 11:13:37 +0200

    anim

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,3 +9,70 @@ const ibg_init = function () {
         }
     }
 }();
+
+const animation_init = function () {
+    const animation = new AnimationListener({
+        params: {
+            reverse: true,
+            mobile: true,
+        },
+
+        selectors: {
+            '.hero-inner__title .title1': {
+                direction: 'bottom',
+            },
+            '.hero-inner__title .title2': {
+                direction: 'bottom',
+                delay: '0.2s',
+            },
+            '.hero-inner__main .main': {
+                direction: 'bottom',
+                delay: '0.3s',
+            },
+            '.hero-inner__main .circle-left-top': {
+                direction: 'left',
+                delay: '0.3s',
+            },
+            '.hero-inner__main .circle-right-middle': {
+                direction: 'right',
+                delay: '0.4s',
+            },
+            '.hero-inner__main .circle-left-bottom': {
+                direction: 'left',
+                delay: '0.5s',
+            },
+            '.hello-block': {
+                direction: 'bottom',
+            },
+            '.hello-block__text': {
+                direction: 'bottom',
+                delay: '0.2s',
+            },
+            '.hello-block__button': {
+                direction: 'bottom',
+                delay: '0.3s',
+            },
+            '.items-list__left .items-block:nth-child(1)': {
+                direction: 'left',
+            },
+            '.items-list__left .items-block:nth-child(2)': {
+                direction: 'left',
+                delay: '0.2s',
+            },
+            '.items-list__right': {
+                direction: 'right',
+                delay: '0.3s',
+            },
+            '.feedback-block:nth-child(1)': {
+                direction: 'left',
+            },
+            '.feedback-block:nth-child(2)': {
+                direction: 'right',
+                delay: '0.2s',
+            },
+            '.footer-inner': {
+                direction: 'bottom',
+            }
+        },
+    });
+}();
